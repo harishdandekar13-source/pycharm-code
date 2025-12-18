ac0c6d159b43502a0038c2dec17f67c155f15820
harishdandekar13-source <harishdandekar13@gmail.com>
2025-12-18 21:42:26 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,17 @@
+# Tuple is also a Type of Data Structure :
+# It is stored in round brackets
+# Tuple creation:
+# Tuples are immutable means we only can slice it or access elements ..!
+# a = {10,20,30,40,50,60,}
+# print("Tuple :",a)
+
+# creating a tuple using round brackets
+# cars = ("BMW","Dodge-Challenger","Bugatti-chiron","Nissan-GTR")
+# # Accessing Elements :
+# print("Tuple :",cars)
+# print("My favourite Car :",cars[2])
+
+# creating a tuple using round brackets
+cars = ("BMW","Dodge-Challenger","Bugatti-chiron","Nissan-GTR")
+# Slicing tuple:
+print("Random Slicing :",cars[1:3])
